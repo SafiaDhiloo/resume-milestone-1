5701e15276246f39ea2cf02b566a1425ac1a3732
Safia Dhiloo <safiadiloo@gmail.com>
2024-11-07 11:37:29 +0500

    static resume

---
+++ script.js
@@ -0,0 +1,4 @@
+var Name = "your Name";
+var Skills = ["HTML", "CSS", "JavaScript", "Python"];
+console.log("Name", Name);
+console.log("Skills", Skills.join(","));
---
+++ script.ts
@@ -0,0 +1,6 @@
+
+const Name:string= "your Name";
+const Skills:string[]=["HTML","CSS","JavaScript","Python"];
+console.log("Name", Name);
+console.log("Skills", Skills.join(","));
+
